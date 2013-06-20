6c235a3094eecbc4893751f9cb71c35f0aafd229
Kayla <kaylaolivia@gmail.com>
2013-06-20 13:14:31 -0400

    Got the CSV output working!!!

--- experiment.py
+++ experiment.py
@@ -12,7 +12,7 @@ changelog:
 '''
 
 # psychopy things
-from psychopy import visual, core, event, misc
+from psychopy import visual, core, event, misc, gui
 
 # things we need to use over and over here for utility
 import numpy as np
@@ -57,22 +57,31 @@ coordinates = [data]
 i, j = data[0]
 daG = GaugeFigure.GaugeFigure(myWin, myMouse, origin=[i, j], radius=50.0)
 
+myDlg = gui.Dlg(title ="PyGauge")
+myDlg.addField('Initials:')
+myDlg.show()
+if gui.OK:
+    thisInfo = myDlg.data
+else: print 'user cancelled'
+
 while len(data) > 0:
     for key in event.getKeys():
         if key in ['escape', 'q']:
             print('done')
             core.quit()
         if key in ['space', 'enter']:
-            tiltInfo.append((theta, phi))
+            tiltInfo.append((theta, phi, i, j))
             del data[0]
             try:
                 i, j = data[0]
                 daG = GaugeFigure.GaugeFigure(myWin, myMouse, origin=[i, j], radius=50.0)
                 continue
             except:
-                with open('pyGaugeExpData.csv', 'rb') as f:
+                with open('pyGaugeOutput.csv', 'rb') as f:
                     bob=list(csv.reader(f))
-                writer=csv.writer(open('pyGaugeExpData.csv','a'))
+                writer=csv.writer(open('pyGaugeOutput.csv','a'))
+                writer.writerow(thisInfo)
+                writer.writerow(('phi', 'theta', 'x', 'y'))
                 for row in bob:
                     writer.writerow(tiltInfo)
                     core.quit()
@@ -83,17 +92,3 @@ while len(data) > 0:
         print theta, phi
     else:
         continue
-
-#we want to add this data to a file... both the coordinates ('data') and the phi and theta values that were entered ('tiltInfo'). we want these in
-# the same file. Also, we will never get to this part of the page (breaking out of the loop) because once the list length = 0, it tries to unpack the list
-# and fails and that crashes the program. so we have 2 things to fix right now.
-
-
-#with open('pyGaugeExpData.csv', 'rb') as f:
-    #data=list(csv.reader(f))
-
-#writer=csv.writer(open('pyGaugeExpData.csv','a'))
-#for row in data:
-        #writer.writerow(testList)
-        #print testList
-        #core.quit()
