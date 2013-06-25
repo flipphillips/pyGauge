0cf653bccea6ff5fedbee9e00d756d9861d5c4a5
kritibehari <kbehari@skidmore.edu>
2013-06-25 10:56:29 -0400

    ALMOST WORKS!!!!!!
    Can't quit politely (without crashing) after it iterates through all
    the images in the test folder

--- experiment.py
+++ experiment.py
@@ -27,7 +27,7 @@ from glob import glob
 
 
 
-
+#rules about hanshtags and such within the data file that provides coordinates for gauge figures
 class CommentedFile:
     def __init__(self, f, commentstring="#"):
         self.f = f
@@ -40,61 +40,86 @@ class CommentedFile:
     def __iter__(self):
         return self
 
-myWin = visual.Window([1000, 1000], monitor='testMonitor', units='pix')
 
-myMouse = event.Mouse(win=myWin)
 
-stimPic=visual.ImageStim(win=myWin, image='/Users/kriti/Desktop/pyGauge/test/1.png')
-#stimDir = "test"
-#stimList = glob("./"+stimDir+'/*.png')
-
-datafile = (CommentedFile(open('File.exp')))
-reader = csv.reader(datafile)
-header = reader.next()
-data = [row for row in reader]
-
-np.random.shuffle(data)
-print data
-
-tiltInfo = []
-coordinates = [data]
-
-i, j = data[0]
-daG = GaugeFigure.GaugeFigure(myWin, myMouse, origin=[i, j], radius=50.0)
-
+#get participant information
 myDlg = gui.Dlg(title ="PyGauge")
 myDlg.addField('Initials:')
 myDlg.show()
 if gui.OK:
     thisInfo = myDlg.data
 else: print 'user cancelled'
+with open('pyGaugeOutput.csv', 'rb') as f:
+    bob=list(csv.reader(f))
+writer=csv.writer(open('pyGaugeOutput.csv','a'))
+writer.writerow(thisInfo)
 
-while len(data) > 0:
-    stimPic.draw()
-    for key in event.getKeys():
-        if key in ['escape', 'q']:
-            print('done')
-            core.quit()
-        if key in ['space', 'enter']:
-            tiltInfo.append((theta, phi, i, j))
-            del data[0]
-            try:
-                i, j = data[0]
-                daG = GaugeFigure.GaugeFigure(myWin, myMouse, origin=[i, j], radius=50.0)
-                continue
-            except:
-                with open('pyGaugeOutput.csv', 'rb') as f:
-                    bob=list(csv.reader(f))
-                writer=csv.writer(open('pyGaugeOutput.csv','a'))
-                writer.writerow(thisInfo)
-                writer.writerow(('phi', 'theta', 'x', 'y'))
-                for row in bob:
-                    writer.writerow(tiltInfo)
-                    core.quit()
-    daG.draw()
-    myWin.flip()
-    if myMouse.getPressed()[0] is 1:
-        (theta, phi) = daG.handleMouseDown()
-        print theta, phi
-    else:
-        continue
+
+stimDir = "test"
+stimList = glob("./"+stimDir+'/*.png')
+
+for stim in stimList:
+    
+
+    myWin = visual.Window([1000, 1000], monitor='testMonitor', units='pix')
+    myMouse = event.Mouse(win=myWin)
+
+    datafile = (CommentedFile(open('File.exp')))
+    reader = csv.reader(datafile)
+    header = reader.next()
+    data = [row for row in reader]
+    
+    np.random.shuffle(data)
+    print data
+    
+    tiltInfo = []
+    coordinates = [data]
+    
+    i, j = data[0]
+    daG = GaugeFigure.GaugeFigure(myWin, myMouse, origin=[i, j], radius=25.0)
+    
+
+    
+    
+    stimPic=visual.ImageStim(win=myWin, image=stim)
+    while len(data) > 0:
+        stimPic.draw()
+        for key in event.getKeys():
+            if key in ['escape', 'q']:
+                print('done')
+                core.quit()
+            if key in ['space', 'enter']:
+                tiltInfo.append((theta, phi, i, j))
+                del data[0]
+                try:
+                    i, j = data[0]
+                    daG = GaugeFigure.GaugeFigure(myWin, myMouse, origin=[i, j], radius=25.0)
+                    continue
+                except:
+                    with open('pyGaugeOutput.csv', 'rb') as f:
+                        bob=list(csv.reader(f))
+                    writer=csv.writer(open('pyGaugeOutput.csv','a'))
+#                    writer.writerow(thisInfo)
+                    writer.writerow(('phi', 'theta', 'x', 'y'))
+                    for row in bob:
+                        writer.writerow(tiltInfo)
+                        continue
+#                        core.quit()
+        daG.draw()
+        myWin.flip()
+        if myMouse.getPressed()[0] is 1:
+            daG.startMouseDown()
+            # monitor mouse and update
+            while myMouse.getPressed()[0] is 1:
+                daG.whileMouseDown()
+                stimPic.draw() #draw image
+                daG.draw()#draw gauge figure
+                myWin.flip()
+                # (theta, phi) = daG.handleMouseDown()
+                (theta, phi) = daG.stopMouseDown()
+                print theta, phi
+    
+        else:
+            continue 
+
+core.quit()
