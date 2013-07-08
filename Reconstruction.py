4e49bbcb3fc4bbccc391c6b80bdd7ddf4ab98d9e
kritibehari <kbehari@skidmore.edu>
2013-07-08 12:31:34 -0400

    Using Pandas to import, working on the math

--- Reconstruction.py
+++ Reconstruction.py
@@ -16,47 +16,43 @@ import GaugeFigure
 import os
 from glob import glob 
 
-#TWO PROBLEMS: 
-# CSV reader reads as strings instead of floats
-# separating the data into 2 lists doesn't work as intended
-
-#get the CSV reader up and running to read the data
-#datafile = open('pyGaugeOutput_okay.csv', 'rU')
 reader = csv.reader(open('pyGaugeOutput_okay.csv', 'rU'))
-#reader.QUOTE_NONNUMERIC
-#header = reader.next()
-data = [row for row in reader]
+image1 = []
+image2 = []
 
 
-print data  
-#
-#make some lists to sort the data by stim
-#image1 = []
-#image2 = []
-#
-#CSV reader loop to sort the data
-#for row in data:
-#    if row[0] == './test/2.png':
-#        image2.append(row[3:4])
-#    elif row[0] == './test/1.png':
-#        image1.append(row[3:4])
-#    else:
-#        raise Exception('Something went wrong!')
-#        break
-#        
-#checking if it worked
-#print image1
-#print image2
-#
-#
+import pandas
+dataFrame = pandas.read_csv('pyGaugeOutput_okay.csv')
+print dataFrame
+
+print dataFrame.describe()
+
+#print dataFrame['x']
+
+#print dataFrame[0:2]
+
+#for row_index, row in dataFrame.iterrows():
+    #print (row['x'] + row['y']) / 2.0
+
+
 #convert slant/tilt settings in a normal vector
-#normalvectors=[]
-#for i in image1:
-#    normalvectors.append(np.cos(image1[1])*np.sin(image1[0]),np.sin(image1[1])*np.sin(image1[0]),np.cos(image1[0]))
-#
+normalvectors=[]
+for row_index, row in dataFrame.iterrows():
+    normalvectors.append(np.cos(row['phi'])*np.sin(row['theta']))
+    normalvectors.append(np.sin(row['phi'])*np.sin(row['theta']))
+    normalvectors.append(np.cos(row['theta']))
+print normalvectors
+
+
 #convert normal vectors in depth gradients
-#gradients=[]
-#for i in image1:
-#    gradients.append((i,1)/(i,3),(i,2)/(i,3))
-#
-#print normalvectors
+gradients=[]
+for row_index, row in dataFrame.iterrows():
+    gradients.append(row['x']/row['theta'])
+    gradients.append(row['y']/row['theta'])
+
+print gradients
+
+#relative depth differences in a triangle: between the first vertex and the other two
+relativedepths=[]
+for row_index, row in dataFrame.iterrows():
+    
