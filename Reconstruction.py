cc752d2a699d73e81055e867d60905ec9c342f08
kritibehari <kbehari@skidmore.edu>
2013-07-19 14:03:42 -0400

    more progress

--- Reconstruction.py
+++ Reconstruction.py
@@ -44,8 +44,6 @@ faces=[[1,2,3],[3,3,1],[2,2,2],[1,3,3]]
 #this is the data that we are using until we have a CSV format / file setup
 count = 0
 relativedepths = []
-bob = []
-jennifer = []
 print len(faces)
 for f in range(len(faces)): #loop for calculating depth gradients for triangle sides
     count = count+1 #used to keep moving through the gradients list later
@@ -62,8 +60,17 @@ for f in range(len(faces)): #loop for calculating depth gradients for triangle s
     relativedepths.append(gx*(x2-x1)+gy*(y2-y1))
     relativedepths.append(gx*(x3-x1)+gy*(y3-y1)) #calculating relative depths and appending to list
     for row in faces: #here we are trying to copy the matlab code with making a list "x" 
-        bob.append(row[0])
-        bob.append(row[1])
-        bob.append(row[0])
-        bob.append(row[1])
+        bob = np.array([[row[0], row[1]],[row[0],row[2]]])
+        print bob
+        break
+    #for i in range(len(relativedepths)-1):
+    #this is the loop for creating a matrix with relative depths and faces -- called "m" in matlab code
+    for i in range(len(bob)): #we can change this length to len(relativedepths) once all the files agree... when they have corresponding values
+        a = np.zeros(len(bob)) #same here
+        b = np.vstack((a, bob))
+        c = np.ones(len(bob))
+        d = np.vstack((b, c))
+        e = np.linalg.inv(d) #in order to take the inverse, we need the data to correspond... otherwise the matrix isnt square and it doesn't work
+        print e 
+
 print relativedepths
